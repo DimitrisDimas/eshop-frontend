8a6ed7eaaa1a321f4426e366c5d990da99afa3f2
Dimitris Dimas <didimasmail@gmail.com>
2025-11-14 19:53:04 +0200

    Switched from an explicit async/await function wrapper to a direct Promise chain.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,40 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import type { Product } from "./app/models/product";
 
 function App() {
 
-  const [products, setProducts] = useState([
-    {id: 1, name: 'Product 1', price: 10},
-    {id: 2, name: 'Product 2', price: 20},
-    {id: 3, name: 'Product 3', price: 30},
-  ]);
+  const [products, setProducts] = useState<Product[]>([]) ;
+
+  /*
+  useEffect(() => {
+    const fetchData = async () => {
+      
+      try{
+        const response = await fetch('http://localhost:8081/api/products') ;
+        if(!response.ok){
+          throw new Error('Failed to fetch the data');
+        }
+
+        const data = await response.json() ;
+        setProducts(data.content) ;
+
+      }catch(error){
+        console.error('Error Fetching Data:', error) ;
+      }
+    };
+
+    fetchData() ;
+  }, []);
+  */
+
+
+  useEffect(() => {
+
+    fetch('http://localhost:8081/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data.content));
+
+  }, []);
 
   return (
     <div>
@@ -15,19 +43,13 @@ function App() {
       {products.map(product => (
         <div key={product.id}>
           <p>Name: {product.name}</p>
+          <p>Description: {product.description}</p>
           <p>Price: {product.price}€</p>
+          <p>Brand: {product.productBrand}</p>
+          <p>Type: {product.productType}</p>
         </div>
       ))}
 
-      <button onClick={() => {
-        setProducts(prevProducts => [
-            ...prevProducts, {id: prevProducts.length+1, name: `Product ${prevProducts.length+1}`, price: Math.floor(Math.random()*100)+1}
-          ]);
-        }}>
-
-        Add Product
-      </button>
-      
     </div>
       
   )
