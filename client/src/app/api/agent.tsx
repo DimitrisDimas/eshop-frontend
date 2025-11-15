93c5718f0d0e584db597e68968cccc25dd06e451
Dimitris Dimas <didimasmail@gmail.com>
2025-11-16 01:37:54 +0200

    Create centralized API agent with axios instance and Store service

---
+++ client/src/app/api/agent.tsx
@@ -0,0 +1,23 @@
+import axios, { type AxiosResponse } from "axios";
+
+axios.defaults.baseURL ='http://localhost:8081/api/';
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+    get: (url: string) => axios.get(url).then(responseBody),
+    post: (url: string, body: object) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: object) =>axios.put(url, body).then(responseBody),
+    delete: (url: string) =>axios.put(url).then(responseBody)
+}
+
+const Store = {    
+    list:()=> requests.get('products'),
+    details:(id: number) => requests.get(`products/${id}`)
+};
+
+const agent = {
+    Store
+}
+
+export default agent;
