3b323f6bb01ebe5c7ff5df3da7bf92d881aae8b4
Dimitris Dimas <didimasmail@gmail.com>
2025-11-16 01:42:30 +0200

    Refactor to use centralized agent for products listing

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,41 +1,30 @@
 import { useEffect, useState } from "react";
 import ProductList from "./ProductList";
 import type { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
 
 export default function Catalog(){
     
     const [products, setProducts] = useState<Product[]>([]) ;
+    const [loading, setLoading] = useState(true) ;
 
+    useEffect(() => {
+        agent.Store.list()
+            .then((products) => setProducts(products.content))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
+        
+    }, []);
+    
+    if(!products) return <h3>Unable to load Products</h3>
+    
     /*
     useEffect(() => {
-        const fetchData = async () => {
-        
-        try{
-            const response = await fetch('http://localhost:8081/api/products') ;
-            if(!response.ok){
-            throw new Error('Failed to fetch the data');
-            }
-
-            const data = await response.json() ;
-            setProducts(data.content) ;
-
-        }catch(error){
-            console.error('Error Fetching Data:', error) ;
-        }
-        };
-
-        fetchData() ;
-    }, []);
-    */
-
-
-    useEffect(() => {
-
         fetch('http://localhost:8081/api/products')
         .then(response => response.json())
         .then(data => setProducts(data.content));
-
     }, []);
+    */
     
     return (
         <>
