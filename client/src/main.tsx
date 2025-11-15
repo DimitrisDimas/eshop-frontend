d7eb9313e8a10dc5a032ebe43bb09e38708a72ff
Dimitris Dimas <didimasmail@gmail.com>
2025-11-15 19:28:12 +0200

    Render app router using RouterProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { router } from './app/router/Routes';
+import { RouterProvider } from "react-router/dom";
+
 import './app/layout/index.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import App from './app/layout/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
