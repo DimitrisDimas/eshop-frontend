479bb129a19a29b60a466c3e184a39632b4811af
Dimitris Dimas <didimasmail@gmail.com>
2025-11-15 14:36:25 +0200

    Switch AppBar from static to fixed and ensure it renders above drawer

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function Header({darkMode, handleThemeChange}: Props){
     return (
-        <AppBar position="static">
+        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1}}>
             <Toolbar>
                 <Typography variant="h6">
                     Koutsianikoulis Sport
