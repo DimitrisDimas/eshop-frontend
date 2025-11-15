ec9f9aea3cc5956395a9b49187906d98e815ee16
Dimitris Dimas <didimasmail@gmail.com>
2025-11-15 20:02:54 +0200

    Add basket icon with badge and account navigation links to header

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,5 +1,6 @@
-import { AppBar, Box, List, ListItem, Switch, Toolbar, Typography} from "@mui/material";
-import { NavLink } from "react-router";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography} from "@mui/material";
+import { Link, NavLink } from "react-router";
 
 const navLinks = [
     {title: 'Home', path:'/'},
@@ -53,6 +54,24 @@ export default function Header({darkMode, handleThemeChange}: Props){
                         </ListItem>
                     ))}
                 </List>
+
+                <Box display='flex' alignItems='center'>
+                    
+                    <IconButton component={Link} to='/basket' size='large' edge='start' color='inherit' sx={{mr:2}}>
+                        <Badge badgeContent="4" color="secondary">
+                            <ShoppingCart/>
+                        </Badge>
+                    </IconButton>
+
+                    <List sx={{display:'flex'}}>
+                        {accountLinks.map(({title, path})=>(
+                            <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                                {title}
+                            </ListItem>
+                        ))}
+                    </List>
+
+                </Box>
 
             </Toolbar>
         </AppBar>
