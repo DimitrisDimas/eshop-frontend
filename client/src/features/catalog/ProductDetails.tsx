4d9aec729a27841fbfc8eefd673bb17095c56916
Dimitris Dimas <didimasmail@gmail.com>
2025-11-15 20:58:45 +0200

    Implement product fetching and loading state using Axios and useEffect

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,7 +1,28 @@
 import { Typography } from "@mui/material";
-
+import { useEffect, useState } from "react";
+import type { Product } from "../../app/models/product";
+import { useParams } from "react-router";
+import axios from "axios";
 
 export default function ProductDetails(){
+    
+    const {id} = useParams<{id:string}>() ;
+
+    const [product, setProduct] = useState<Product | null>() ;
+    const [loading, setLoading] = useState(true) ;
+
+    useEffect( () => {
+        
+        axios.get(`http://localhost:8081/api/products/${id}`)
+            .then(response => setProduct(response.data))
+            .catch(error => console.error(error))
+            .finally(() => setLoading(false));
+
+    }, [id])
+
+    if (loading) return <h3>Loading Product...</h3>
+    if (!product) return <h3>Product not found</h3>
+
     return (
         <Typography variant="h2">Product Details</Typography>
     )
