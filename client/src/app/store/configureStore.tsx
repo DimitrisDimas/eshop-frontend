734bd121092787a7f3adfba608041eb42659c362
Dimitris Dimas <didimasmail@gmail.com>
2025-11-16 23:40:13 +0200

    Add route for /basket to display the BasketPage component.

--- client/src/app/store/configureStore.tsx
+++ client/src/app/store/configureStore.tsx
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit' 
 import type { TypedUseSelectorHook } from "react-redux";
 import { useDispatch, useSelector } from "react-redux";
+import { basketSlice } from '../../features/basket/basketSlice';
 
 export const store = configureStore({
     reducer:{
